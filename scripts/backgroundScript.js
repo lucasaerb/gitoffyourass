137f936d6c21f9ebfecbfca1cfd221bc945e4de1
Nisarg Shah <npshah9856@gmail.com>
2020-02-01 14:51:43 -0800

    Functional popup for timing things

--- scripts/backgroundScript.js
+++ scripts/backgroundScript.js
@@ -1,27 +1,54 @@
 let delay = 1 //default is 5 seconds
 
-//Listerners
-//On runtime message 
+const onMessage = message => {
+  chrome.alarms.create("second", {when: Date.now() + 1000})
+}
+
+const decrementTime = () => {
+  const s = parseInt(localStorage.getItem('seconds'),10)
+  const m = parseInt(localStorage.getItem('minutes'),10)
+  const h = parseInt(localStorage.getItem('hours'),10)
+
+  localStorage.setItem('seconds',s <= 0 ? 0 : s - 1)
+
+  if(m > 0){
+    localStorage.setItem('seconds', 59)
+    localStorage.setItem('minutes', m-1)
+    return;
+  }
+
+  if(h > 0){
+    localStorage.setItem('seconds', 59)
+    localStorage.setItem('minutes', 59)
+    localStorage.setItem('hours', h- 1)
+
+    return;
+  }
+}
+
+const getTimeLeftMs = () => {
+  const s = parseInt(localStorage.getItem('seconds'),10)
+  const m = parseInt(localStorage.getItem('minutes'),10)
+  const h = parseInt(localStorage.getItem('hours'),10)
+  return (s + (m* 60) + (h* 60)) * 1000
+}
+
+const onAlarm = alarm => {
+  console.log(alarm)
+  chrome.alarms.clear("second")
+  decrementTime()
+  const timeLeft = getTimeLeftMs()
+  console.log(timeLeft)
+  if (timeLeft === 0){
+      console.log("DONE!")
+      chrome.runtime.sendMessage({done: true})
+      localStorage.setItem('cancel', false)
+      // chrome.tabs.create({url: 'index.html'});
+      return;
+  }
+  chrome.runtime.sendMessage({updateTime: true})
+  chrome.alarms.create("second", {when: Date.now() + 1000})
+}
+
 chrome.runtime.onMessage.addListener(onMessage)
-//On alarm
 chrome.alarms.onAlarm.addListener(onAlarm)
-
-// chrome.browserAction.onClicked.addListener(function() {
-  // });
-  
-chrome.alarms.create("alarm", {when: Date.now() + delay*1000})
-
-function onMessage(message){
-  delay = parseInt(message.seconds,10)
-}
-
-function onAlarm(){
-    console.log("Hi")
-    console.log(delay)
-    // Delete all the timers 
-    chrome.alarms.clearAll()
-    // And start a new timer now
-    chrome.tabs.create({url: 'index.html'});
-
-    // chrome.alarms.create("alarm", {when: Date.now() + delay*1000})
-}
--- scripts/popupScript.js
+++ scripts/popupScript.js
@@ -1,14 +1,141 @@
-// const seconds = document.getElementById("seconds")
-// const hours = document.getElementById('timer_hours')
+const hours = document.getElementById('timer_hours')
+const minutes = document.getElementById('timer_minutes')
+const seconds = document.getElementById('timer_seconds')
 
-// hours.addEventListener('change', event => {
+const start = document.getElementById('timer_start')
+const cancel = document.getElementById('timer_stop')
 
-// })
+const text = document.getElementById('text')
 
-// seconds.value = (localStorage.getItem('seconds') || 5)
+const initTime = () => {
+    const h = parseInt(localStorage.getItem('hours'),10) || 0
+    const m = parseInt(localStorage.getItem('minutes'),10) || 0
+    const s = parseInt(localStorage.getItem('seconds'),10) || 0
 
-// seconds.addEventListener('change',(event) => {
-//     const value = event.target.value
-//     localStorage.setItem("seconds", value)
-//     chrome.runtime.sendMessage({seconds:value})
-// })
+    hours.value = h === 0 ? parseInt(localStorage.getItem('permhours'),10) || 0 : h
+    minutes.value = m === 0 ? parseInt(localStorage.getItem('permminutes'),10) || 0 : m
+    seconds.value = s === 0 ? parseInt(localStorage.getItem('permseconds'),10) || 0 : s
+}
+
+const initButtons = () => {
+    const cancelVal = localStorage.getItem('cancel') || false
+    if (cancelVal === "true"){
+        start.className = "button disabled"
+        cancel.className = "button"  
+    }
+    else{
+        start.className = "button"
+        cancel.className = "button disabled"   
+    }
+}
+
+const init = () => {
+    initTime()
+    initButtons()
+}
+
+init()
+
+const updateTime = () => {
+    hours.value = parseInt(localStorage.getItem('hours'),10)
+    minutes.value = parseInt(localStorage.getItem('minutes'),10)
+    seconds.value = parseInt(localStorage.getItem('seconds'),10)
+}
+
+
+const onMessage = message => {
+    if (message.updateTime){
+        updateTime()
+        initButtons()
+    }
+    if (message.done){
+        hours.value = parseInt(localStorage.getItem('permhours'),10)
+        minutes.value = parseInt(localStorage.getItem('permminutes'),10)
+        seconds.value = parseInt(localStorage.getItem('permseconds'),10)
+        start.className = "button"
+        cancel.className = "button disabled"   
+    }
+}
+
+const onStart = event => {
+    const h = parseInt(hours.value, 10)
+    const m = parseInt(minutes.value, 10)
+    const s = parseInt(seconds.value, 10)
+
+    localStorage.setItem('hours', h)
+    localStorage.setItem('minutes', m)
+    localStorage.setItem('seconds', s)
+    localStorage.setItem('permhours', h)
+    localStorage.setItem('permminutes', m)
+    localStorage.setItem('permseconds', s)
+    localStorage.setItem('cancel', true)
+    console.log(h,":",m,":",s)
+    cancel.className = "button"
+    start.className = "button disabled"
+    text.innerHTML = "GET READY TO WORK!!"
+
+    chrome.runtime.sendMessage({hours:h, minutes: m, seconds:s})
+}
+
+const onCancel = event => {
+    text.innerHTML = ""
+    start.className = "button"
+    cancel.className = "button disabled"
+    chrome.alarms.clear("second")
+    hours.value = parseInt(localStorage.getItem('permhours'),10)
+    minutes.value = parseInt(localStorage.getItem('permminutes'),10)
+    seconds.value = parseInt(localStorage.getItem('permseconds'),10)
+    localStorage.setItem('cancel', false)
+}
+
+const onTimeChange = (value, hour, minute, second) => {
+    const val = parseInt(value, 10)
+    if (isNaN(val)|| val < 0){
+        if(hour){
+            hours.value = 0
+        }
+        else{
+            if (minute){
+                minutes.value = 0
+            }
+            else{
+                seconds.value = 0
+            }
+        }
+    }
+    else if ((hour && val > 23) || ((minute || second) && val > 59)){
+        if(hour){
+            hours.value = 23
+        }
+        else{
+            if (minute){
+                minutes.value = 59
+            }
+            else{
+                seconds.value = 59
+            }
+        }
+    }
+    else{
+        if(hour){
+            hours.value = val
+        }
+        else{
+            if (minute){
+                minutes.value = val
+            }
+            else{
+                seconds.value = val
+            }
+        }
+    }
+}
+
+hours.addEventListener('keyup', event => onTimeChange(event.target.value, true, false, false))
+minutes.addEventListener('keyup', event => onTimeChange(event.target.value, false, true, false))
+seconds.addEventListener('keyup', event => onTimeChange(event.target.value, false, false, true))
+
+start.addEventListener('click', onStart)
+cancel.addEventListener('click', onCancel)
+
+chrome.runtime.onMessage.addListener(onMessage)
