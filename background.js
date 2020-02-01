31e55dea1db6b36eaaa939fe65a5927f6ee8520b
Nisarg Shah <npshah9856@gmail.com>
2020-01-31 23:27:21 -0800

    added a periodic alarm

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
-'use strict';
-
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.create({url: 'index.html'});
   });
+  
+chrome.alarms.create("alarm", {when: Date.now() + 5000})
+
+chrome.alarms.onAlarm.addListener(onAlarm)
+
+function onAlarm(){
+    console.log("Hi")
+    // Delete all the timers 
+    chrome.alarms.clearAll()
+    // And start a new timer now
+    chrome.alarms.create("alarm", {when: Date.now() + 5000})
+}
